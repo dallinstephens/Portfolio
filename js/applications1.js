9f0de44bb599d21afca63c5f3029055a38e409f6
gospelvideoquotes <dallins@juno.com>
2017-06-07 11:49:03 -0600

    Angular Script now works!

--- js/applications1.js
+++ js/applications1.js
@@ -1,3 +1,5 @@
+document.getElementById("my-result-box").innerHTML = '<div ng-app="myApp" ng-controller="nameController"><div ng-include="\'pl/ng1/applications1.myresult.html\'"></div></div>';
+
 var app = angular.module("myApp", []);
 app.controller("nameController",
 function($scope) {
