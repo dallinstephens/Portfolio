e22b78bb5cb8faf8f89d6edc174f2fe22b34adf5
gospelvideoquotes <dallins@juno.com>
2017-06-08 03:37:40 -0600

    put back body hide in pages.js

--- js/pages.js
+++ js/pages.js
@@ -1,6 +1,8 @@
 // This script runs the function the page loads. Initially, the "Primary Code Tidbit" is showing and the mode is in "portrait".
 var w3codecolor;
 $(document).ready(function(){
+	$("#body").hide();
+	
 	$("#applications1").mouseover(function(){
 		applications1();
 		$("#tidbit-box").load("pl/ng1/applications1.tidbit.html");
