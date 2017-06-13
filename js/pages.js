f642b7d617ba21b448d9eddefeaf89b44d49c583
gospelvideoquotes <dallins@juno.com>
2017-06-13 14:20:17 -0600

    Put in $(this).click(); because triggering did not seem to be working

--- js/pages.js
+++ js/pages.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 	// Used for loading my external hrefs upon mouseover
 	$('#myPanel').on('mouseover click', 'li a', function(event) {
-		$(this).trigger('click');
+		$(this).click();
 		$(".parent").hide(); // Hides start.html in iframe
 		// Testing: use alert($(this).index('li a'));
 		var href = $(this).attr('href'); // Stores href from a tag
