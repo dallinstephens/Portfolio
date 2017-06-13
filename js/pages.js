168fb659d82f3ab7f27727317d63c4fe3e07f420
gospelvideoquotes <dallins@juno.com>
2017-06-13 16:15:58 -0600

    mouseover 1st?

--- js/pages.js
+++ js/pages.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
 $("#body").show(function() {
 
-		setTimeout(function() {
+		//setTimeout(function() {
 			// There needs to be a very slight delay after loading the html so the height and w3 color loads correctly.
 			var href_length_minus_12 = href.length-5-7; // href length minus the beginning part pl/ng1/ (7 characters) and minus the ending .html (4 characters)
 	    var topic = href.substr(7, href_length_minus_12); // href minus the beginning part pl/ng1/ (starts at character 7) and minus the ending .html
@@ -49,7 +49,7 @@ $("#body").show(function() {
 
 			// loads path.tidbit.html and runs w3CodeColor after loading
 			w3CodeColor();
-		}, 1);
+		//}, 1);
 		//});
 		//var mycode_html = ".mycode.html";
 		//var mycode_url = path.concat(mycode_html); // Concatenates: path.mycode.html
